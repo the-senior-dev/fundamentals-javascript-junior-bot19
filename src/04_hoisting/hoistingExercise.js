dbaf4234e6367ff991238a74b2f97c04216508c4
bot19 <jsbot@pm.me>
2025-08-15 11:53:03 +1000

    DONE 04 hoisting

--- src/04_hoisting/hoistingExercise.js
+++ src/04_hoisting/hoistingExercise.js
@@ -1,9 +1,9 @@
 // Fix the code below to make the function return 12
 function calculate() {
-    a = 4;
-    let a;
-    b = 3;
-    return a * b; // Should return 12
+  a = 4;
+  var a;
+  b = 3;
+  return a * b; // Should return 12
 }
 
 module.exports = calculate;
